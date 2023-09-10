1fdfa09e999dcbe5d10748d4ce4fd2e530ef1d1b
muthee-p <pruemu9@gmail.com>
2023-09-10 18:57:44 +0300

    almost done

--- components/Car.tsx
+++ components/Car.tsx
@@ -1,39 +1,39 @@
-'use client'
+// 'use client'
 
-import React, { useEffect, useRef, useState} from 'react';
-import Image from 'next/image'
+// import React, { useEffect, useRef, useState} from 'react';
+// import Image from 'next/image'
 
-const Car = () => {
-  const carRef = useRef(null);
-  const [ scrollPosition, setScrollPosition] = useState(0)
+// const Car = () => {
+//   const carRef = useRef(null);
+//   const [ scrollPosition, setScrollPosition] = useState(0)
 
-const handleScroll = () => {
-  const currentSroll = window.scrollY;
-  setScrollPosition(currentSroll)
+// const handleScroll = () => {
+//   const currentSroll = window.scrollY;
+//   setScrollPosition(currentSroll)
 
-  carRef.current.style.transform = `${currentSroll}px`;
-}
+//   carRef.current.style.transform = `${currentSroll}px`;
+// }
 
-useEffect(() => {
-  window.addEventListener('scroll', handleScroll);
-  return () => {
-    window.removeEventListener('scroll', handleScroll)
-  };
-}, []);
+// useEffect(() => {
+//   window.addEventListener('scroll', handleScroll);
+//   return () => {
+//     window.removeEventListener('scroll', handleScroll)
+//   };
+// }, []);
 
-  return (
-    <div ref={carRef} className=' car'
-      style={{top:`${scrollPosition}px`}}>
-      <Image
-                    src='/car.svg'
-                    alt='car'
-                    className='shake'
-                    width={60}
-                    height={60}
-                    priority
-                />
-    </div>
-  );
-};
+//   return (
+//     <div ref={carRef} className=' car'
+//       style={{top:`${scrollPosition}px`}}>
+//       <Image
+//                     src='/car.svg'
+//                     alt='car'
+//                     className='shake'
+//                     width={60}
+//                     height={60}
+//                     priority
+//                 />
+//     </div>
+//   );
+// };
 
-export default Car;
+// export default Car;
